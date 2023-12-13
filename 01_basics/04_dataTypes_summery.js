c6a1038900410d41e4836861905fc1297f5d799d
Sagar-ghasal <153616757+Sagar-ghasal@users.noreply.github.com>
2023-12-13 06:53:11 +0000

    04 datatype summer

---
+++ 01_basics/04_dataTypes_summery.js
@@ -0,0 +1,27 @@
+//# primitive Data Type in Javascript()
+//7 types:String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// # Reference DataType (Non primitive)
+// Array, object, functions
+
+//javascript is dynamically typed language
+
+// Symbol data type example
+// const id = Symbol("123");
+// const anotherId = Symbol("123");
+// console.log(id===anotherId);
+
+//BigInt data type example
+// const bigNumber = 315557448775n;
+//console.log(typeof(bigNumber))
+
+
+//Array, objects, function example
+
+const heros =["shaktiman", "naagraj", "doga"]
+const myFunction = function(){
+    console.log("Hello world")
+}
+console.log(typeof heros)
+
+
