101ce9ecf00614acd0f3ec655d83c6f5c1504ff6
Sagar-ghasal <153616757+Sagar-ghasal@users.noreply.github.com>
2023-12-28 09:15:37 +0000

    test.js

---
+++ 11_fun_with_js/test.js
@@ -0,0 +1,14 @@
+const myArr =[];
+
+//PACKED_SMI_ELEMENTS defult type only take numbers
+const arrTwo = [1,2,3,4,5]
+
+//PACKED_DOUBLE
+arrTwo(6.25)
+
+//PACKED-ELEMENTS
+arrTwo.push("7")
+
+
+//HOLEY_ELEMENTS
+arrTwo[10] = 11
